8d339f2977b49a261e3e31a05fbbddb048dec7f1
Sergey Kirichenko <whole-white-whale@yandex.ru>
2024-03-31 19:32:47 +0300

    Create an audio data class

---
+++ melody_generator/audio.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from dataclasses import dataclass
+from scipy.io import wavfile
+
+
+@dataclass(frozen=True, slots=True)
+class Audio:
+    melody: str
+
+    def dump(self, path: str):
+        samples, t = [], np.linspace(0.0, 1.0, 48000)
+
+        for note in self.melody:
+            sample = np.sin(2 * np.pi * get_frequency(note) * t)
+            samples.append(sample[: int(len(sample) / 3)])
+
+        wavfile.write(path, 48000, np.concatenate(samples))
+
+
+def get_frequency(note: str) -> float:
+    frequency = 0.0
+
+    match note:
+        case "1":
+            frequency = 440.00
+
+        case "2":
+            frequency = 493.88
+
+        case "3":
+            frequency = 523.25
+
+        case "4":
+            frequency = 587.33
+
+        case "5":
+            frequency = 659.25
+
+        case "6":
+            frequency = 698.46
+
+        case "7":
+            frequency = 392.00
+
+    return frequency
