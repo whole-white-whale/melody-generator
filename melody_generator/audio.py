dfbbf40b1e1a4eb7f5ab85fc55bc8509d331dc47
Sergey Kirichenko <whole-white-whale@yandex.ru>
2024-04-02 15:15:20 +0300

    Make linter pass

--- melody_generator/audio.py
+++ melody_generator/audio.py
@@ -1,25 +1,34 @@
-import numpy as np
+"""Модуль для работы с аудиофайлами."""
 
 from dataclasses import dataclass
+
+import numpy as np
+
 from numpy.typing import NDArray
 from scipy.io import wavfile
 
 
 @dataclass(frozen=True, slots=True)
 class Audio:
+    """Аудиофайл."""
+
     melody: str
     sample_rate: int
     speed: int
 
     def dump(self, path: str):
+        """Записать аудиофайл."""
+
         wavfile.write(path, self.sample_rate, self.get_wave())
 
     def get_wave(self) -> NDArray[np.float64]:
-        return np.concatenate(
-            [self.get_sample(note) for note in self.melody], dtype=np.float64
-        )
+        """Получить аудиосигнал."""
+
+        return np.concatenate([self.get_sample(note) for note in self.melody])
 
     def get_sample(self, note: str) -> NDArray[np.float64]:
+        """Получить сэмпл данной ноты."""
+
         duration = 15.0 / self.speed
 
         return (
@@ -42,6 +51,8 @@ class Audio:
         )
 
     def get_frequency(self, note: str) -> float:
+        """Получить частоту данной ноты."""
+
         frequencies = {
             "1": 440.00,
             "2": 493.88,
