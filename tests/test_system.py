8b08f330dffe09b2b12813eb2a03230746415f1a
Sergey Kirichenko <162910887+whole-white-whale@users.noreply.github.com>
2024-04-02 14:39:04 +0300

    Revert "Create a system from configuration method"
    This reverts commit 44db313c8c5ce59d086a7af6473dec7af2c55f27.

--- tests/test_system.py
+++ tests/test_system.py
@@ -3,32 +3,6 @@ from melody_generator.system import System
 
 
 class TestSystem:
-    def test_from_configuration(self):
-        system = System.from_configuration(
-            [
-                "1 ->",
-                "2 -> 2",
-                "3 -> 4 5",
-                "6 -> 7 8 9",
-            ]
-        )
-
-        have = system.symbols
-
-        want = {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-        }
-
-        assert have == want
-
     def test_produce(self):
         symbols = {"s", "t"}
 
